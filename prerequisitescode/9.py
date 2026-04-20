286615ef18d492f5a32c79176e0799461f666904
jataleshivam <shivamjatale123@gmail.com>
2026-04-20 11:25:37 +0530

    Update 9.py

--- prerequisitescode/9.py
+++ prerequisitescode/9.py
@@ -1,22 +1,16 @@
-import sys
+def is_palindrome(n):
+    s = str(n)
+    return s == s[::-1]
 
-while True:
-    try:
-        a = input()
-        b = input()
-    except EOFError:
-        break
+t = int(input())
 
-    freq1 = [0]*26
-    freq2 = [0]*26
+for _ in range(t):
+    n = int(input())
+    count = 0
 
-    for ch in a:
-        freq1[ord(ch)-97] += 1
-    for ch in b:
-        freq2[ord(ch)-97] += 1
+    while not is_palindrome(n):
+        rev = int(str(n)[::-1])
+        n += rev
+        count += 1
 
-    result = ""
-    for i in range(26):
-        result += chr(i+97) * min(freq1[i], freq2[i])
-
-    print(result)
+    print(count, n)
